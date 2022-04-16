4f3222973cb80f1113ecfc2598cd2d5195ff044b
Artjom <arzarv@ttu.ee>
2022-04-16 22:15:58 +0300

    bookmarks added + fixes

--- streaming/js/main.js
+++ streaming/js/main.js
@@ -21,3 +21,147 @@ function setPictureYellow() {
 function setPictureWhite() {
     document.getElementById("profile-dropdown-pic").src = '../images/icons/white profile icon.png'
 }
+
+function setBookmark() {
+    let imgPath = document.getElementById("bookmarkIcon").src
+    if (imgPath.includes('bookmark-white.png')) {
+        document.getElementById("bookmarkIcon").src = '../images/icons/bookmark-yellow.png'
+    } else if (imgPath.includes('bookmark-yellow.png')) {
+        document.getElementById("bookmarkIcon").src = '../images/icons/bookmark-white.png'
+    }
+}
+
+function setBookmark1() {
+    let imgPath = document.getElementById("bookmarkIcon1").src
+    if (imgPath.includes('bookmark-white.png')) {
+        document.getElementById("bookmarkIcon1").src = '../images/icons/bookmark-yellow.png'
+    } else if (imgPath.includes('bookmark-yellow.png')) {
+        document.getElementById("bookmarkIcon1").src = '../images/icons/bookmark-white.png'
+    }
+}
+
+function setBookmark2() {
+    let imgPath = document.getElementById("bookmarkIcon2").src
+    if (imgPath.includes('bookmark-white.png')) {
+        document.getElementById("bookmarkIcon2").src = '../images/icons/bookmark-yellow.png'
+    } else if (imgPath.includes('bookmark-yellow.png')) {
+        document.getElementById("bookmarkIcon2").src = '../images/icons/bookmark-white.png'
+    }
+}
+
+function setBookmark3() {
+    let imgPath = document.getElementById("bookmarkIcon3").src
+    if (imgPath.includes('bookmark-white.png')) {
+        document.getElementById("bookmarkIcon3").src = '../images/icons/bookmark-yellow.png'
+    } else if (imgPath.includes('bookmark-yellow.png')) {
+        document.getElementById("bookmarkIcon3").src = '../images/icons/bookmark-white.png'
+    }
+}
+
+function setBookmark4() {
+    let imgPath = document.getElementById("bookmarkIcon4").src
+    if (imgPath.includes('bookmark-white.png')) {
+        document.getElementById("bookmarkIcon4").src = '../images/icons/bookmark-yellow.png'
+    } else if (imgPath.includes('bookmark-yellow.png')) {
+        document.getElementById("bookmarkIcon4").src = '../images/icons/bookmark-white.png'
+    }
+}
+
+function setBookmark5() {
+    let imgPath = document.getElementById("bookmarkIcon5").src
+    if (imgPath.includes('bookmark-white.png')) {
+        document.getElementById("bookmarkIcon5").src = '../images/icons/bookmark-yellow.png'
+    } else if (imgPath.includes('bookmark-yellow.png')) {
+        document.getElementById("bookmarkIcon5").src = '../images/icons/bookmark-white.png'
+    }
+}
+
+function setBookmark6() {
+    let imgPath = document.getElementById("bookmarkIcon6").src
+    if (imgPath.includes('bookmark-white.png')) {
+        document.getElementById("bookmarkIcon6").src = '../images/icons/bookmark-yellow.png'
+    } else if (imgPath.includes('bookmark-yellow.png')) {
+        document.getElementById("bookmarkIcon6").src = '../images/icons/bookmark-white.png'
+    }
+}
+
+function setBookmark7() {
+    let imgPath = document.getElementById("bookmarkIcon7").src
+    if (imgPath.includes('bookmark-white.png')) {
+        document.getElementById("bookmarkIcon7").src = '../images/icons/bookmark-yellow.png'
+    } else if (imgPath.includes('bookmark-yellow.png')) {
+        document.getElementById("bookmarkIcon7").src = '../images/icons/bookmark-white.png'
+    }
+}
+
+function setBookmark8() {
+    let imgPath = document.getElementById("bookmarkIcon8").src
+    if (imgPath.includes('bookmark-white.png')) {
+        document.getElementById("bookmarkIcon8").src = '../images/icons/bookmark-yellow.png'
+    } else if (imgPath.includes('bookmark-yellow.png')) {
+        document.getElementById("bookmarkIcon8").src = '../images/icons/bookmark-white.png'
+    }
+}
+
+function setBookmark9() {
+    let imgPath = document.getElementById("bookmarkIcon9").src
+    if (imgPath.includes('bookmark-white.png')) {
+        document.getElementById("bookmarkIcon9").src = '../images/icons/bookmark-yellow.png'
+    } else if (imgPath.includes('bookmark-yellow.png')) {
+        document.getElementById("bookmarkIcon9").src = '../images/icons/bookmark-white.png'
+    }
+}
+
+function setBookmark10() {
+    let imgPath = document.getElementById("bookmarkIcon10").src
+    if (imgPath.includes('bookmark-white.png')) {
+        document.getElementById("bookmarkIcon10").src = '../images/icons/bookmark-yellow.png'
+    } else if (imgPath.includes('bookmark-yellow.png')) {
+        document.getElementById("bookmarkIcon10").src = '../images/icons/bookmark-white.png'
+    }
+}
+
+function setBookmark11() {
+    let imgPath = document.getElementById("bookmarkIcon11").src
+    if (imgPath.includes('bookmark-white.png')) {
+        document.getElementById("bookmarkIcon11").src = '../images/icons/bookmark-yellow.png'
+    } else if (imgPath.includes('bookmark-yellow.png')) {
+        document.getElementById("bookmarkIcon11").src = '../images/icons/bookmark-white.png'
+    }
+}
+
+function setBookmark12() {
+    let imgPath = document.getElementById("bookmarkIcon12").src
+    if (imgPath.includes('bookmark-white.png')) {
+        document.getElementById("bookmarkIcon12").src = '../images/icons/bookmark-yellow.png'
+    } else if (imgPath.includes('bookmark-yellow.png')) {
+        document.getElementById("bookmarkIcon12").src = '../images/icons/bookmark-white.png'
+    }
+}
+
+function setBookmark13() {
+    let imgPath = document.getElementById("bookmarkIcon13").src
+    if (imgPath.includes('bookmark-white.png')) {
+        document.getElementById("bookmarkIcon13").src = '../images/icons/bookmark-yellow.png'
+    } else if (imgPath.includes('bookmark-yellow.png')) {
+        document.getElementById("bookmarkIcon13").src = '../images/icons/bookmark-white.png'
+    }
+}
+
+function setBookmark14() {
+    let imgPath = document.getElementById("bookmarkIcon14").src
+    if (imgPath.includes('bookmark-white.png')) {
+        document.getElementById("bookmarkIcon14").src = '../images/icons/bookmark-yellow.png'
+    } else if (imgPath.includes('bookmark-yellow.png')) {
+        document.getElementById("bookmarkIcon14").src = '../images/icons/bookmark-white.png'
+    }
+}
+
+function setBookmark15() {
+    let imgPath = document.getElementById("bookmarkIcon15").src
+    if (imgPath.includes('bookmark-white.png')) {
+        document.getElementById("bookmarkIcon15").src = '../images/icons/bookmark-yellow.png'
+    } else if (imgPath.includes('bookmark-yellow.png')) {
+        document.getElementById("bookmarkIcon15").src = '../images/icons/bookmark-white.png'
+    }
+}
