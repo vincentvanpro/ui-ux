5a2ae852701c475a476ac19067ce222eee41f81d
angulo <angulo@ttu.ee>
2022-04-17 06:49:24 +0300

    responsive initial pages[steps for registration ànd login page] + separated сss files for them

--- streaming/js/main.js
+++ streaming/js/main.js
@@ -1,19 +1,3 @@
-let loginBtn = document.getElementById("loginBtn");
-let accountBtn = document.getElementById("accountBtn");
-
-if(loginBtn) {
-loginBtn.onclick = function openLoginPage() {
-        location.href = "login.html";
-    };
-}
-
-if(accountBtn) {
-document.getElementById("accountBtn").onclick = function openAccount() {
-        location.href = "account.html";
-    };
-}
-
-
 function setPictureYellow() {
     document.getElementById("profile-dropdown-pic").src = '../images/icons/yellow profile icon.png'
 }
